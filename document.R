1e0aa2187a5c75ec470d9a2bb9f18b73cab2603b
Maia Sosa Kapur <kapurm@uw.edu>
2021-03-10 16:20:55 -0800

    code for loading model(s) & rendering document

---
+++ document.R
@@ -0,0 +1,26 @@
+## i went in and ensured rtools40 was the first line in my PATH per https://github.com/nwfsc-assess/sa4ss/issues/15
+# install.packages('installr') ## this is outdated
+require(sa4ss)
+require(here)
+require(rmarkdown)
+rmarkdown::pandoc_version() ## ‘2.8.0.1’ has bib issues
+## had to paste from C:\Users\mkapur\AppData\Local\Pandoc to C:\Program Files\RStudio\bin\pandoc
+# rmarkdown::find_pandoc(version = "2.11.0.4")
+# rmarkdown::find_pandoc(dir = "~/Downloads/Pandoc")
+# pandoc_available(version = "2.11.0.4", error = FALSE)
+
+# sa4ss::draft(authors = c("Maia S. Kapur"), 
+#              species = "Sablefish",
+#              latin = "Anoplopoma fimbria",
+#              create_dir = TRUE)
+
+setwd("doc")
+
+## extract base case into doc and save as model
+model <- read_model(mod_loc ="C:/Users/mkapur/Dropbox/UW/assessments/sab-2021/200.00_base_files_1July_ToSTAR",
+                   save_loc  = getwd())
+
+bookdown::render_book("00a.Rmd",
+                      clean = FALSE, 
+                      output_dir =here())
+sessionInfo()
