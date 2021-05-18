bb40affebc68447dc0ef094b4ea701dce3eda0a6
Maia Sosa Kapur <kapurm@uw.edu>
2021-05-17 17:20:54 -0700

    port out extra stuff about data sources

---
+++ dontuse/extraData.R
@@ -0,0 +1,9 @@
+---
+title: "extraData"
+output: html_document
+---
+
+### Alaska Fisheries Science Center Slope Survey
+ \begin{comment} The parameterization differed from that used for the WCGBT Survey in the following three ways (see Appendix \ref{app:vast} for more details): no random component for vessel-year was included, 150 knots were used for the spatial component, and the encounter probability was fixed at one for any year where all tows encountered the species. The estimated index shows an increase from 1999 to 2001 (Figure \ref{AFSCSLPindexest}). Q-Q plots suggested that the gamma distribution (Figures \ref{AFSCSLPQQ} and \ref{QQhistAFSCSLP}) fit the data, better than a log-normal distribution (results not shown). The highest densities for this survey were predicted off the coast of Washington (Figure \ref{surveydensAFSCSLP1}). No spatial or spatiotemporal patterns were found in the Pearson residuals
+(Figures \ref{surveyresid1AFSCSLP1} and \ref{surveyresid2AFSCSLP1}).
+Similar to the NWFSC Slope Survey biological data, the length compositions for the AFSC Slope Survey show a strong 1999 cohort, a few age-0 fish in 2000 and 2001, and dimorphic growth (Figure \ref{AFSCSLPlendat}). The marginal age compositions are similar as well, with the exception of a seemingly anomalous number of males at the largest sizes (Figure \ref{AFSCSLPagedat}). \end{comment}
